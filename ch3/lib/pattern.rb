c100b6f2bd70bc9043d3e2ee74000a497dc43bc6
Josh Cheek <josh.cheek@gmail.com>
2017-06-26 21:20:16 -0500

    Parses a lot of shit

--- ch3/lib/pattern.rb
+++ ch3/lib/pattern.rb
@@ -1,3 +1,156 @@
-module Pattern
+require 'treetop'
 
+
+Treetop.load_from_string <<~GRAMMAR
+grammar PatternGrammar
+  rule pattern
+    expr:(nondeterministic_expr / deterministic_expr) {
+      def to_ast
+        Pattern::Top.new expr.to_ast
+      end
+    }
+  end
+
+  rule nondeterministic_expr
+    (left:deterministic_expr '|' right:(nondeterministic_expr / deterministic_expr)) {
+      def to_ast
+        Pattern::Either.new(left.to_ast, right.to_ast)
+      end
+    }
+  end
+
+  rule deterministic_expr
+    (first:modified_expr rest:deterministic_expr) {
+      def to_ast
+        Pattern::Sequence.new(
+          first.to_ast,
+          rest.to_ast,
+        )
+      end
+    }
+    /
+    (first:unmodified_expr rest:deterministic_expr) {
+      def to_ast
+        Pattern::Sequence.new(
+          first.to_ast,
+          rest.to_ast,
+        )
+      end
+    }
+    /
+    expr:modified_expr {
+      def to_ast
+        expr.to_ast
+      end
+    }
+    /
+    expr:unmodified_expr {
+      def to_ast
+        expr.to_ast
+      end
+    }
+  end
+
+  rule modified_expr
+    (expr:unmodified_expr mod:modifier) {
+      def to_ast
+        mod.to_ast expr.to_ast
+      end
+    }
+  end
+
+  rule unmodified_expr
+    char:char {
+      def to_ast
+        char.to_ast
+      end
+    }
+  end
+
+  rule modifier
+    zom:zero_or_more {
+      def to_ast(modifyee)
+        zom.to_ast(modifyee)
+      end
+    }
+  end
+
+  rule zero_or_more
+    '*' {
+      def to_ast(modifyee)
+        Pattern::ZeroOrMore.new(modifyee)
+      end
+    }
+  end
+
+  rule char
+    [a-zA-Z0-9] {
+      def to_ast
+        Pattern::ExactMatch.new(text_value)
+      end
+    }
+  end
+end
+GRAMMAR
+
+module Pattern
+  extend self
+
+  def parse(str)
+    return Top.new Eos.new if str.empty?
+    grammar = PatternGrammarParser.new
+    tree = grammar.parse(str)
+    if tree
+      tree.to_ast
+    else
+      puts "\e[31m#{grammar.failure_reason}\e[0m"
+      require "pry"
+      binding.pry
+    end
+    # ast.the_e.empty?             # => false
+    # ast.text_value               # => "e"
+    # ast.elements                 # => [SyntaxNode offset=0, "e"]
+    # ast.terminal?                # => false
+    # ast.nonterminal?             # => true
+    # to_parse = str.chars
+  end
+
+  class Top < Struct.new(:pattern)
+    def inspect
+      "Top.new(#{pattern.tree_inspect})"
+    end
+  end
+
+  class Eos
+    def ==(other)
+      Eos === other
+    end
+    def tree_inspect
+      'Eos.new'
+    end
+  end
+
+  class ExactMatch < Struct.new(:char)
+    def tree_inspect
+      "ExactMatch.new(#{char.inspect})"
+    end
+  end
+
+  class Sequence < Struct.new(:left, :right)
+    def tree_inspect
+      "Sequence.new(#{left.tree_inspect}, #{right.tree_inspect})"
+    end
+  end
+
+  class ZeroOrMore < Struct.new(:expr)
+    def tree_inspect
+      "ZeroOrMore.new(#{expr.tree_inspect})"
+    end
+  end
+
+  class Either < Struct.new(:left, :right)
+    def tree_inspect
+      "Either.new(#{left.tree_inspect}, #{right.tree_inspect})"
+    end
+  end
 end
--- ch3/spec/parse_spec.rb
+++ ch3/spec/parse_spec.rb
@@ -5,91 +5,109 @@ RSpec.configure do |config|
   config.formatter = 'documentation'
 end
 
-RSpec.describe 'Pattern.parse' do
-  include Pattern
+module Pattern
+  RSpec.describe 'Pattern.parse' do
+    include Pattern
 
-  def parses!(str, expected)
-    actual = Pattern.parse(str)
-    expect(actual).to eq expected
-  end
-
-  specify 'empty string -> EOS' do
-    parses! "", Eos.new
-  end
-
-  specify 'a single alphanumeric char -> ExactMatch' do
-    [*"a".."z", *"A".."Z", *"0".."9"].each do |char|
-      parses! char.dup, ExactMatch.new(char.dup)
+    def parses!(str, expected)
+      actual = Pattern.parse(str)
+      expect(actual).to eq Pattern::Top.new(expected)
     end
-  end
 
-  specify 'multiple characters -> Sequence of ExactMatch' do
-    parses! "abc", Sequence.new([
-      ExactMatch.new("a"),
-      ExactMatch.new("b"),
-      ExactMatch.new("c"),
-    ])
-  end
+    specify 'empty string -> EOS' do
+      parses! "", Eos.new
+    end
 
-  specify 'parens -> Group'
+    specify 'a single alphanumeric char -> ExactMatch' do
+      [*"a".."z", *"A".."Z", *"0".."9"].each do |char|
+        parses! char.dup, ExactMatch.new(char.dup)
+      end
+    end
 
-  specify 'asterisks to ZeroOrMore of whatever is to the left' do
-    parses! 'a*', ZeroOrMore.new(ExactMatch.new("a"))
-  end
-
-  specify 'asterisks bind higher than sequences' do
-    parses! 'ab*', Sequence.new(
-      ExactMatch.new("a"),
-      ZeroOrMore.new(ExactMatch.new("b"))
-    )
-  end
-
-  specify 'pipes to Either' do
-    parses! "a|b", Either.new(
-      ExactMatch.new("a"),
-      ExactMatch.new("b"),
-    )
-  end
-
-  specify 'sequences bind tighter than pipes' do
-    parses! "ab|cd", Either.new(
-      Sequence.new([
+    specify 'multiple characters -> Sequence of ExactMatch' do
+      parses! "abcd", Sequence.new(
         ExactMatch.new("a"),
-        ExactMatch.new("b"),
-      ]),
-      Sequence.new([
-        ExactMatch.new("c"),
-        ExactMatch.new("d"),
-      ]),
-    )
-  end
-
-  specify 'parens -> group' do
-    parses! "(a)", Group.new(ExactMatch.new("a"))
-  end
-
-  specify 'parens bind tighter than sequenes' do
-    parses! "a(bc)d", Sequence.new([
-      ExactMatch.new("a"),
-      Group.new(
         Sequence.new(
           ExactMatch.new("b"),
-          ExactMatch.new("c"),
-        ),
-      ),
-      ExactMatch.new("d"),
-    ])
-    parses! 'a(b|c)*d', Sequence.new([
-      ExactMatch.new("a"),
-      ZeroOrMore.new(
-        Group.new(
-          Either.new(
-            ExactMatch.new("b"),
+          Sequence.new(
             ExactMatch.new("c"),
+            ExactMatch.new("d"),
           )
         )
-      ),
-      ExactMatch.new("d"),
-    ])
+      )
+    end
+
+    specify 'parens -> Group'
+
+    specify 'asterisks to ZeroOrMore of whatever is to the left' do
+      parses! 'a*', ZeroOrMore.new(ExactMatch.new("a"))
+    end
+
+    specify 'asterisks bind higher than sequences' do
+      parses! 'ab*', Sequence.new(
+        ExactMatch.new("a"),
+        ZeroOrMore.new(ExactMatch.new("b"))
+      )
+    end
+
+    specify 'pipes to Either' do
+      parses! "a|b", Either.new(
+        ExactMatch.new("a"),
+        ExactMatch.new("b"),
+      )
+      parses! "a|b|c", Either.new(
+        ExactMatch.new("a"),
+        Either.new(
+          ExactMatch.new("b"),
+          ExactMatch.new("c"),
+        )
+      )
+    end
+
+    specify 'sequences bind tighter than pipes' do
+      parses! "ab|cd", Either.new(
+        Sequence.new(
+          ExactMatch.new("a"),
+          ExactMatch.new("b"),
+        ),
+        Sequence.new(
+          ExactMatch.new("c"),
+          ExactMatch.new("d"),
+        ),
+      )
+    end
+
+    specify 'parens -> group' do
+      parses! "(a)", Group.new(ExactMatch.new("a"))
+    end
+
+    specify 'parens bind tighter than sequenes' do
+      parses! "a(bc)d", Sequence.new(
+        ExactMatch.new("a"),
+        Sequence.new(
+          Group.new(
+            Sequence.new(
+              ExactMatch.new("b"),
+              ExactMatch.new("c"),
+            ),
+          ),
+          ExactMatch.new("d"),
+        )
+      )
+      parses! 'a(b|c)*d', Sequence.new(
+        ExactMatch.new("a"),
+        Sequence.new(
+          ZeroOrMore.new(
+            Group.new(
+              Either.new(
+                ExactMatch.new("b"),
+                ExactMatch.new("c"),
+              )
+            )
+          ),
+          ExactMatch.new("d"),
+        )
+      )
+    end
   end
 end
