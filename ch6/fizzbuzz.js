eaa6ebecd205994dedb49c20b27f4e578b2db30b
Josh Cheek <josh.cheek@gmail.com>
2017-07-25 02:42:30 -0500

    Write it recursively

--- ch6/fizzbuzz.js
+++ ch6/fizzbuzz.js
@@ -1,5 +1,10 @@
-for(let i = 1; i < 100; ++i) {
-  if(i%15 == 0)
+// INTEROP (translates lambda calculus into JavaScript)
+
+// LAMBDA CALCULUS
+function fizzBuzz(i, max) {
+  if(i == max)
+    return
+  else if(i%15 == 0)
     console.log('FizzBuzz')
   else if(i%5 == 0)
     console.log('Fizz')
@@ -7,4 +12,6 @@ for(let i = 1; i < 100; ++i) {
     console.log('Buzz')
   else
     console.log(i)
+  fizzBuzz(i+1, max)
 }
+fizzBuzz(1, 100)
