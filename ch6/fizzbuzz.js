2bd531fb9a117d97627adb78a22357a4ed58f796
Josh Cheek <josh.cheek@gmail.com>
2017-07-25 21:44:23 -0500

    Fizzzz Buzzzz Refactor

--- ch6/fizzbuzz.js
+++ ch6/fizzbuzz.js
@@ -18,47 +18,47 @@ const ASSERT_EQUAL = (expected, actual) => {
 (cons =>
 (DO => (y =>
 (pred => (isZero => (sub => (numEq => (lt => (mod =>
-{
-  y(recur => i => max =>
-    IF(numEq(i)(max))
-      (_=>_) // base case
-      (_=>
-        DO(_=>
-          IF(isZero(mod(i)(n15)))
-            (_=>PRINT('FizzBuzz'))
-            (_=>IF(isZero(mod(i)(n5)))
-              (_=>PRINT('Fizz'))
-              (_=>IF(isZero(mod(i)(n3)))
-                (_=>PRINT('Buzz'))
-                (_=>PRINT(TO_I(i))))))
-          (_=>recur(succ(i))(max))))
-  (n1)(n100)
-
-  // tests
-  ASSERT_EQUAL('a', IF(TRUE)(_=>'a')(_=>'b'))
-  ASSERT_EQUAL('b', IF(FALSE)(_=>'a')(_=>'b'))
-  ASSERT_EQUAL(2, TO_I(mod(n5)(n3)));
-  ASSERT_EQUAL(3, TO_I(mod(n3)(n5)));
-  ASSERT_EQUAL(10, TO_I(mod(n100)(n15)));
-  ASSERT(cons(true)(false)(a => b => a))
-  ASSERT(cons(false)(true)(a => b => b))
-  ASSERT_EQUAL(100, TO_I(n100));
-  ASSERT_EQUAL(4, pred(n5)(n => n + 1)(0))
-  ASSERT_EQUAL(10, TO_I(sub(n15)(n5)))
-  ASSERT(TO_BOOL(AND(TRUE)(TRUE)))
-  REFUTE(TO_BOOL(AND(TRUE)(FALSE)))
-  REFUTE(TO_BOOL(AND(FALSE)(TRUE)))
-  REFUTE(TO_BOOL(AND(FALSE)(FALSE)))
-  ASSERT(TO_BOOL(numEq(n0)(n0)))
-  REFUTE(TO_BOOL(numEq(n1)(n0)))
-  REFUTE(TO_BOOL(numEq(n0)(n1)))
-  ASSERT(TO_BOOL(numEq(n5)(n5)))
-  REFUTE(TO_BOOL(numEq(n3)(n1)))
-  REFUTE(TO_BOOL(lt(n3)(n3)))
-  REFUTE(TO_BOOL(lt(n5)(n5)))
-  ASSERT(TO_BOOL(lt(n3)(n5)))
-  REFUTE(TO_BOOL(lt(n5)(n3)))
-})(
+  DO
+    (_=> { // TESTS
+      ASSERT_EQUAL('a', IF(TRUE)(_=>'a')(_=>'b'))
+      ASSERT_EQUAL('b', IF(FALSE)(_=>'a')(_=>'b'))
+      ASSERT_EQUAL(2, TO_I(mod(n5)(n3)));
+      ASSERT_EQUAL(3, TO_I(mod(n3)(n5)));
+      ASSERT_EQUAL(10, TO_I(mod(n100)(n15)));
+      ASSERT(cons(true)(false)(a => b => a))
+      ASSERT(cons(false)(true)(a => b => b))
+      ASSERT_EQUAL(100, TO_I(n100));
+      ASSERT_EQUAL(4, pred(n5)(n => n + 1)(0))
+      ASSERT_EQUAL(10, TO_I(sub(n15)(n5)))
+      ASSERT(TO_BOOL(AND(TRUE)(TRUE)))
+      REFUTE(TO_BOOL(AND(TRUE)(FALSE)))
+      REFUTE(TO_BOOL(AND(FALSE)(TRUE)))
+      REFUTE(TO_BOOL(AND(FALSE)(FALSE)))
+      ASSERT(TO_BOOL(numEq(n0)(n0)))
+      REFUTE(TO_BOOL(numEq(n1)(n0)))
+      REFUTE(TO_BOOL(numEq(n0)(n1)))
+      ASSERT(TO_BOOL(numEq(n5)(n5)))
+      REFUTE(TO_BOOL(numEq(n3)(n1)))
+      REFUTE(TO_BOOL(lt(n3)(n3)))
+      REFUTE(TO_BOOL(lt(n5)(n5)))
+      ASSERT(TO_BOOL(lt(n3)(n5)))
+      REFUTE(TO_BOOL(lt(n5)(n3)))})
+    (_=> // FIZZ BUZZ
+      (fizzbuzz => fizzbuzz(n1)(n100))
+      (y(recur => i => max =>
+        IF(numEq(i)(max))
+          (_=>_) // base case
+          (_=>
+            DO(_=>
+              IF(isZero(mod(i)(n15)))
+                (_=>PRINT('FizzBuzz'))
+                (_=>IF(isZero(mod(i)(n5)))
+                  (_=>PRINT('Fizz'))
+                  (_=>IF(isZero(mod(i)(n3)))
+                    (_=>PRINT('Buzz'))
+                    (_=>PRINT(TO_I(i))))))
+            (_=>recur(succ(i))(max))))))
+)(
   // mod
   y(recur => n1 => n2 =>
     IF(lt(n1)(n2))(_=>n1)(_=>recur(sub(n1)(n2))(n2))
