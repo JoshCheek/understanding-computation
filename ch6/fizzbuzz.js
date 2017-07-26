3f9a09594b1b100544b32391fcc8b3fea8fb1dea
Josh Cheek <josh.cheek@gmail.com>
2017-07-25 21:54:01 -0500

    Refactor `DO` to be infinitely recursive`

--- ch6/fizzbuzz.js
+++ ch6/fizzbuzz.js
@@ -16,48 +16,47 @@ const ASSERT_EQUAL = (expected, actual) => {
 (succ => (add => (n0 => (n1 => (n3 => (n5 => (n15 => (n100 =>
 (TRUE => (FALSE => (IF => (AND =>
 (cons =>
-(DO => (y =>
+(y => (DO =>
 (pred => (isZero => (sub => (numEq => (lt => (mod =>
-  DO
-    (_=> { // TESTS
-      ASSERT_EQUAL('a', IF(TRUE)(_=>'a')(_=>'b'))
-      ASSERT_EQUAL('b', IF(FALSE)(_=>'a')(_=>'b'))
-      ASSERT_EQUAL(2, TO_I(mod(n5)(n3)));
-      ASSERT_EQUAL(3, TO_I(mod(n3)(n5)));
-      ASSERT_EQUAL(10, TO_I(mod(n100)(n15)));
-      ASSERT(cons(true)(false)(a => b => a))
-      ASSERT(cons(false)(true)(a => b => b))
-      ASSERT_EQUAL(100, TO_I(n100));
-      ASSERT_EQUAL(4, pred(n5)(n => n + 1)(0))
-      ASSERT_EQUAL(10, TO_I(sub(n15)(n5)))
-      ASSERT(TO_BOOL(AND(TRUE)(TRUE)))
-      REFUTE(TO_BOOL(AND(TRUE)(FALSE)))
-      REFUTE(TO_BOOL(AND(FALSE)(TRUE)))
-      REFUTE(TO_BOOL(AND(FALSE)(FALSE)))
-      ASSERT(TO_BOOL(numEq(n0)(n0)))
-      REFUTE(TO_BOOL(numEq(n1)(n0)))
-      REFUTE(TO_BOOL(numEq(n0)(n1)))
-      ASSERT(TO_BOOL(numEq(n5)(n5)))
-      REFUTE(TO_BOOL(numEq(n3)(n1)))
-      REFUTE(TO_BOOL(lt(n3)(n3)))
-      REFUTE(TO_BOOL(lt(n5)(n5)))
-      ASSERT(TO_BOOL(lt(n3)(n5)))
-      REFUTE(TO_BOOL(lt(n5)(n3)))})
-    (_=> // FIZZ BUZZ
-      (fizzbuzz => fizzbuzz(n1)(n100))
-      (y(recur => i => max =>
-        IF(numEq(i)(max))
-          (_=>_) // base case
-          (_=>
-            DO(_=>
-              IF(isZero(mod(i)(n15)))
-                (_=>PRINT('FizzBuzz'))
-                (_=>IF(isZero(mod(i)(n5)))
-                  (_=>PRINT('Fizz'))
-                  (_=>IF(isZero(mod(i)(n3)))
-                    (_=>PRINT('Buzz'))
-                    (_=>PRINT(TO_I(i))))))
-            (_=>recur(succ(i))(max))))))
+  DO // TESTS
+    (_=>ASSERT_EQUAL('a', IF(TRUE)(_=>'a')(_=>'b')))
+    (_=>ASSERT_EQUAL('b', IF(FALSE)(_=>'a')(_=>'b')))
+    (_=>ASSERT_EQUAL(2, TO_I(mod(n5)(n3))))
+    (_=>ASSERT_EQUAL(3, TO_I(mod(n3)(n5))))
+    (_=>ASSERT_EQUAL(10, TO_I(mod(n100)(n15))))
+    (_=>ASSERT(cons(true)(false)(a => b => a)))
+    (_=>ASSERT(cons(false)(true)(a => b => b)))
+    (_=>ASSERT_EQUAL(100, TO_I(n100)))
+    (_=>ASSERT_EQUAL(4, pred(n5)(n => n + 1)(0)))
+    (_=>ASSERT_EQUAL(10, TO_I(sub(n15)(n5))))
+    (_=>ASSERT(TO_BOOL(AND(TRUE)(TRUE))))
+    (_=>REFUTE(TO_BOOL(AND(TRUE)(FALSE))))
+    (_=>REFUTE(TO_BOOL(AND(FALSE)(TRUE))))
+    (_=>REFUTE(TO_BOOL(AND(FALSE)(FALSE))))
+    (_=>ASSERT(TO_BOOL(numEq(n0)(n0))))
+    (_=>REFUTE(TO_BOOL(numEq(n1)(n0))))
+    (_=>REFUTE(TO_BOOL(numEq(n0)(n1))))
+    (_=>ASSERT(TO_BOOL(numEq(n5)(n5))))
+    (_=>REFUTE(TO_BOOL(numEq(n3)(n1))))
+    (_=>REFUTE(TO_BOOL(lt(n3)(n3))))
+    (_=>REFUTE(TO_BOOL(lt(n5)(n5))))
+    (_=>ASSERT(TO_BOOL(lt(n3)(n5))))
+    (_=>REFUTE(TO_BOOL(lt(n5)(n3))))
+    // FIZZ BUZZ
+    (_=>(fizzbuzz => fizzbuzz(n1)(n100))
+        (y(recur => i => max =>
+          IF(numEq(i)(max))
+            (_=>_) // base case
+            (_=>
+              DO(_=>
+                IF(isZero(mod(i)(n15)))
+                  (_=>PRINT('FizzBuzz'))
+                  (_=>IF(isZero(mod(i)(n5)))
+                    (_=>PRINT('Fizz'))
+                    (_=>IF(isZero(mod(i)(n3)))
+                      (_=>PRINT('Buzz'))
+                      (_=>PRINT(TO_I(i))))))
+              (_=>recur(succ(i))(max))))))
 )(
   // mod
   y(recur => n1 => n2 =>
@@ -82,12 +81,13 @@ const ASSERT_EQUAL = (expected, actual) => {
           (_=>cons(FALSE)(f(value)))))
      (cons(TRUE)(arg))
      (isFirst => value => value)
+))(// DO
+  y(recur => a => b => recur((_=>_=>_)(b(a(_=>_)))))
 ))(
   // y combinator
   f =>
     (builder => arg => f(builder(builder))(arg))
     (builder => arg => f(builder(builder))(arg))
-))(a => b => b(a(_=>_))                                              // DO
 ))(a => b => f => f(a)(b)                                            // cons
 ))(a => b => a(b)(a)                                                 // AND
 ))(bool => trueCase => falseCase => bool(trueCase)(falseCase)(_=>_)  // IF
